7212ca0963b64d9f3ac77594eeea744dddd4a649
glenjoy99 <glenjoy99@gmail.com>
2019-07-05 10:27:25 -0400

    Integrated weather-js API

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@
 console.log("Bot started");
 
 var Twit = require('twit');
-var Weather = require('weather.js');
+var weather = require('weather-js');
 
 var T = new Twit({
   consumer_key:         'TQyMiBduZKDuzYok50G5FLgWA',
@@ -12,3 +12,22 @@ var T = new Twit({
   access_token:         '1146897859519946753-r2eEsCO69OeF3QmtD7KCI8eG1Rby7l',
   access_token_secret:  'LBFBeNEbsfiJ5O5tztf0nlsvhibbSr4ZSVhdT9PnerbIF',
 })
+
+var out;
+var curr;
+
+weather.find({search: 'Gaithersburg, MD', degreeType: 'F'}, function(err, result) {
+  if(err) console.log(err);
+
+  //console.log(JSON.stringify(result, null, 2));
+  //out = JSON.parse(result);
+
+  curr = result[0].current.temperature;
+  //console.log(curr);
+  var str = "It is currently " + curr + " degrees Fahrenheit in Gaithersburg";
+
+  T.post('statuses/update', { status: str }, function(err, data, response) {
+    //console.log(data)
+    console.log('Tweeted!');
+  })
+});
