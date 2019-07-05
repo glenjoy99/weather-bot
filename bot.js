a0122ee115357f2bc6c2a4d2a76867f911ac6328
glenjoy99 <glenjoy99@gmail.com>
2019-07-05 15:14:07 -0400

    Added daily forecast feature

--- bot.js
+++ bot.js
@@ -29,4 +29,41 @@ var tweetCurrWeather = function () {
   });
 }
 
+var today;
+var todayHigh;
+var todayLow;
+
+var tweetTodayForecast = function () {
+  weather.find({search: 'Gaithersburg, MD', degreeType: 'F'}, function(err, result) {
+    if(err) console.log(err);
+    today = result[0].current.day;
+    if (today == "Monday") {
+      todayHigh = result[0].forecast[0].high;
+      todayLow = result[0].forecast[0].low;
+    }
+    if (today == "Tuesday") {
+      todayHigh = result[1].forecast[1].high;
+      todayLow = result[1].forecast[1].low;
+    }
+    if (today == "Wednesday") {
+      todayHigh = result[2].forecast[2].high;
+      todayLow = result[2].forecast[2].low;
+    }
+    if (today == "Thursday") {
+      todayHigh = result[3].forecast[3].high;
+      todayLow = result[3].forecast[3].low;
+    }
+    if (today == "Friday") {
+      todayHigh = result[4].forecast[4].high;
+      todayLow = result[4].forecast[4].low;
+    }
+    var str = "The forecast today is a high of " + todayHigh + " and a low of " + todayLow + " degrees Fahrenheit in Gaithersburg";
+    T.post('statuses/update', { status: str }, function(err, data, response) {
+      console.log('Tweeted!');
+    })
+  });
+}
+
+
 setInterval(tweetCurrWeather, 10800000); //tweets every 3 hours
+setInterval(tweetTodayForecast, 32400000); //tweets every 9 hours
