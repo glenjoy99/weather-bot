5cb94e930a1ba62a52ba23cba78cfa5665ff14c6
glenjoy99 <glenjoy99@gmail.com>
2019-07-09 21:13:53 -0400

    Added config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'TQyMiBduZKDuzYok50G5FLgWA',
+  consumer_secret:      'knnCJ7KyvjGJlgnb3fGuEi5wTyccbijJDhVLkZuVZf7Fax5vnv',
+  access_token:         '1146897859519946753-r2eEsCO69OeF3QmtD7KCI8eG1Rby7l',
+  access_token_secret:  'LBFBeNEbsfiJ5O5tztf0nlsvhibbSr4ZSVhdT9PnerbIF',
+}
