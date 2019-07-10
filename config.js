feb619e29cc50eba96ed6b0eb0e57a1aa8956ac1
glenjoy99 <glenjoy99@gmail.com>
2019-07-09 21:42:51 -0400

    Security features added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'TQyMiBduZKDuzYok50G5FLgWA',
-  consumer_secret:      'knnCJ7KyvjGJlgnb3fGuEi5wTyccbijJDhVLkZuVZf7Fax5vnv',
-  access_token:         '1146897859519946753-r2eEsCO69OeF3QmtD7KCI8eG1Rby7l',
-  access_token_secret:  'LBFBeNEbsfiJ5O5tztf0nlsvhibbSr4ZSVhdT9PnerbIF',
+  consumer_key:         process.env.BOT_CONSUMER_KEY,
+  consumer_secret:      process.env.BOT_CONSUMER_SECRET,
+  access_token:         process.env.BOT_ACCESS_TOKEN,
+  access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET,
 }
